b2f87c1730d70cc618435abe656eb39141f472da
mufidussani <sanimufidus@gmail.com>
2022-04-21 16:10:47 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        dark: '#0f172a',
+        semidark: '#64748b',
+        textdark: 'text-slate-300'
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
